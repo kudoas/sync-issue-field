ef9f6fe97c8516b43a9d120326eb91e6dd00e512
Daichi Kudo <sakanactor624@gmail.com>
2023-10-11 01:49:03 +0900

    ⚙️ split getting parent issue attribute query

--- issue.go
+++ issue.go
@@ -34,3 +34,69 @@ func (i *IssueID) GetIssueID() githubv4.ID {
 func (i *IssueID) GetParentIssueID() githubv4.ID {
 	return i.Repository.Issue.TrackedInIssues.Nodes[0].ID
 }
+
+type ParentIssue struct {
+	Node struct {
+		Issue struct {
+			Assignees struct {
+				Nodes []struct {
+					ID githubv4.ID
+				}
+			} `graphql:"assignees(first: 5)"`
+			Labels struct {
+				Nodes []struct {
+					ID githubv4.ID
+				}
+			} `graphql:"labels(first: 20)"`
+			Milestone struct {
+				ID githubv4.ID
+			} `graphql:"milestone"`
+			ProjectItems struct {
+				Nodes []struct {
+					FieldValues struct {
+						Nodes []struct {
+							OnProjectV2ItemFieldSingleSelectValue struct {
+								ID       githubv4.ID
+								OptionID githubv4.ID
+							} `graphql:"... on ProjectV2ItemFieldSingleSelectValue"`
+							OnProjectV2ItemFieldDateValue struct {
+								ID   githubv4.ID
+								Date githubv4.String
+							} `graphql:"... on ProjectV2ItemFieldDateValue"`
+						}
+					} `graphql:"fieldValues(last: 10)"`
+				}
+			} `graphql:"projectItems(first: 5)"`
+		} `graphql:"... on Issue"`
+	} `graphql:"node(id: $issueID)"`
+}
+
+func NewParentIssue() *ParentIssue {
+	return &ParentIssue{}
+}
+
+func (p *ParentIssue) Query(client *githubv4.Client, ctx context.Context, variables map[string]interface{}) error {
+	return client.Query(context.Background(), &p, variables)
+}
+
+func (p *ParentIssue) GetAssigneeIDs() *[]githubv4.ID {
+	return extractIDs(p.Node.Issue.Assignees.Nodes)
+}
+
+func (p *ParentIssue) GetLabelIDs() *[]githubv4.ID {
+	return extractIDs(p.Node.Issue.Labels.Nodes)
+}
+
+func (p *ParentIssue) GetMilestoneID() *githubv4.ID {
+	return &p.Node.Issue.Milestone.ID
+}
+
+func extractIDs(nodes []struct {
+	ID githubv4.ID
+}) *[]githubv4.ID {
+	ids := make([]githubv4.ID, len(nodes))
+	for i := range nodes {
+		ids[i] = nodes[i].ID
+	}
+	return &ids
+}
--- main.go
+++ main.go
@@ -34,42 +34,8 @@ func main() {
 		log.Fatalf("failed to get issue id: %v", err)
 	}
 
-	var getParentIssueQuery struct {
-		Node struct {
-			Issue struct {
-				Assignees struct {
-					Nodes []struct {
-						ID githubv4.ID
-					}
-				} `graphql:"assignees(first: 5)"`
-				Labels struct {
-					Nodes []struct {
-						ID githubv4.ID
-					}
-				} `graphql:"labels(first: 20)"`
-				Milestone struct {
-					ID githubv4.ID
-				} `graphql:"milestone"`
-				ProjectItems struct {
-					Nodes []struct {
-						FieldValues struct {
-							Nodes []struct {
-								OnProjectV2ItemFieldSingleSelectValue struct {
-									ID       githubv4.ID
-									OptionID githubv4.ID
-								} `graphql:"... on ProjectV2ItemFieldSingleSelectValue"`
-								OnProjectV2ItemFieldDateValue struct {
-									ID   githubv4.ID
-									Date githubv4.String
-								} `graphql:"... on ProjectV2ItemFieldDateValue"`
-							}
-						} `graphql:"fieldValues(last: 10)"`
-					}
-				} `graphql:"projectItems(first: 5)"`
-			} `graphql:"... on Issue"`
-		} `graphql:"node(id: $issueID)"`
-	}
-	if err := client.Query(context.Background(), &getParentIssueQuery, map[string]interface{}{
+	p := NewParentIssue()
+	if err := p.Query(client, context.Background(), map[string]interface{}{
 		"issueID": githubv4.ID(i.GetParentIssueID()),
 	}); err != nil {
 		log.Fatalf("failed to get parent issue: %v", err)
@@ -85,21 +51,11 @@ func main() {
 
 	input := githubv4.UpdateIssueInput{
 		ID:          i.GetIssueID(),
-		AssigneeIDs: extractIDs(getParentIssueQuery.Node.Issue.Assignees.Nodes),
-		LabelIDs:    extractIDs(getParentIssueQuery.Node.Issue.Labels.Nodes),
-		MilestoneID: &getParentIssueQuery.Node.Issue.Milestone.ID,
+		AssigneeIDs: p.GetAssigneeIDs(),
+		LabelIDs:    p.GetLabelIDs(),
+		MilestoneID: p.GetMilestoneID(),
 	}
 	if err := client.Mutate(context.Background(), &mutation, input, nil); err != nil {
 		log.Fatalf("failed to update issue: %v", err)
 	}
-}
-
-func extractIDs(nodes []struct {
-	ID githubv4.ID
-}) *[]githubv4.ID {
-	ids := make([]githubv4.ID, len(nodes))
-	for i := range nodes {
-		ids[i] = nodes[i].ID
-	}
-	return &ids
 }
