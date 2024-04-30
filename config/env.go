7efd80ff5c5c9708958f6f983352334f0a0914e6
Daichi Kudo <45157831+kudoas@users.noreply.github.com>
2024-04-30 21:27:44 +0900

    feat: validate env config (#33)

---
+++ config/env.go
@@ -0,0 +1,75 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Env struct {
+	token       string
+	repoOwner   string
+	repoName    string
+	issueNumber int
+}
+
+var (
+	token      = os.Getenv("INPUT_TOKEN")
+	repository = os.Getenv("INPUT_REPOSITORY")
+	issue      = os.Getenv("INPUT_ISSUE")
+)
+
+type EnvInvalidError struct {
+	message string
+}
+
+func (e *EnvInvalidError) Error() string {
+	return fmt.Sprintf("environment variable invalid error: %v", e.message)
+}
+
+func ProvideEnv() (*Env, error) {
+	r := strings.Split(repository, "/")
+	var (
+		repoOwner = r[0]
+		repoName  = r[1]
+	)
+	if (repoOwner == "" || repoName == "") || len(r) != 2 {
+		return nil, &EnvInvalidError{"Repository format is incorrect"}
+	}
+
+	if token == "" {
+		return nil, &EnvInvalidError{"Token is not set"}
+	}
+
+	issue, err := strconv.Atoi(issue)
+	if err != nil {
+		return nil, &EnvInvalidError{"Issue number can't parse to int"}
+	}
+	if issue == 0 {
+		return nil, &EnvInvalidError{"Issue number is not set"}
+	}
+
+	return &Env{
+		token:       token,
+		repoOwner:   repoOwner,
+		repoName:    repoName,
+		issueNumber: issue,
+	}, nil
+}
+
+func (e *Env) Token() string {
+	return e.token
+}
+
+func (e *Env) RepoOwner() string {
+	return e.repoOwner
+}
+
+func (e *Env) RepoName() string {
+	return e.repoName
+}
+
+func (e *Env) IssueNumber() int {
+	return e.issueNumber
+}
